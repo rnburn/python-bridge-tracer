00784ed43f66ad9f32f956dfa15c6eeeb1206b90
Ryan Burn <ryan.burn@gmail.com>
2019-05-08 22:42:04 -0700

    Style fixes.

--- src/lib/utility.h
+++ src/lib/utility.h
@@ -4,12 +4,12 @@
 
 namespace python_bridge_tracer {
 template <class T>
-static void* toVoidPtr(T* t) noexcept {
+void* toVoidPtr(T* t) noexcept {
   return reinterpret_cast<void*>(t);
 }
 
 template <class T>
-static void* toVoidPtr(const T* t) noexcept {
+void* toVoidPtr(const T* t) noexcept {
   return toVoidPtr(const_cast<T*>(t));
 }
 
